05de50403f0ca33bb42c771d83cb98aba44aba53
Joe Gallotta <joeygallotta@gmail.com>
2018-11-11 13:41:07 -0600

    add api update and destroy actions

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,12 +3,10 @@ class Api::UsersController < ApplicationController
     @users = User.all
     render "index.json.jbuilder"
   end
-
   def show
     @user = User.find_by(id: params[:id])
     render "show.json.jbuilder"
   end
-
   def create
     user = User.new(
       first_name: params[:first_name],
@@ -21,5 +19,20 @@ class Api::UsersController < ApplicationController
     else
       render json: {errors: user.errors.full_messages}, status: :bad_request
     end
+  end
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.update(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number]
+    )
+    render "show.json.jbuilder"
+  end
+  def destroy
+    @user = User.find_by(id: params[:id])
+    @user.destroy
+    render json: {message: "You just deleted the item"}
   end
 end
