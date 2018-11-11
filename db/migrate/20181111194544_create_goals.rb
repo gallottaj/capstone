92d01f2cf9cd628fc00c5f5d19d65dc3bfd1c976
Joe Gallotta <joeygallotta@gmail.com>
2018-11-11 13:46:29 -0600

    generate Goal model

---
+++ db/migrate/20181111194544_create_goals.rb
@@ -0,0 +1,11 @@
+class CreateGoals < ActiveRecord::Migration[5.2]
+  def change
+    create_table :goals do |t|
+      t.string :goal_name
+      t.string :due_date
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_08_023830) do
+ActiveRecord::Schema.define(version: 2018_11_11_194544) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "goals", force: :cascade do |t|
+    t.string "goal_name"
+    t.string "due_date"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
