0dbf466a552cfe824b4ec9f0ad47ec6c5b6c197a
Joe Gallotta <joeygallotta@gmail.com>
2018-11-28 18:52:37 -0600

    add create, update and destroy actions to track_habits controller

--- app/controllers/api/track_habits_controller.rb
+++ app/controllers/api/track_habits_controller.rb
@@ -1,9 +1,4 @@
 class Api::TrackHabitsController < ApplicationController
-  # a user should be able to create a habit they want to track
-  # a user should be able to update a habit
-  # a user should be able to destroy a habit
-  # a user should be able to see all habits
-  # a user should be able to see a specific habit
   def index
     @track_habits = TrackHabit.all
     render "index.json.jbuilder"
@@ -12,42 +7,35 @@ class Api::TrackHabitsController < ApplicationController
     @track_habit = TrackHabit.find_by(id: params[:id])
     render "show.json.jbuilder"
   end
-  # def create
-  #   todo = Todo.new(
-  #     task_name: params[:task_name],
-  #     task_description: params[:task_description],
-  #     # sub_goal_id: params[:sub_goal_id],
-  #     task_checkbox_status: params[:task_checkbox_status],
-  #     journal_id: params[:journal_id],
-  #   )
-  #   if todo.save
-  #     render json: {message: 'todo created successfully'}, status: :created
-  #   else
-  #     render json: {errors: todo.errors.full_messages}, status: :bad_request
-  #   end
-  # end
-  # def update
-  #   @todo = Todo.find_by(id: params[:id])
-  #   @todo.update(
-  #     task_name: params[:task_name],
-  #     task_description: params[:task_description],
-  #     # sub_goal_id: params[:sub_goal_id],
-  #     task_checkbox_status: params[:task_checkbox_status],
-  #     journal_id: params[:journal_id],
-  #   )
-  #   render "show.json.jbuilder"
-  # end
-  # def destroy
-  #   @todo = Todo.find_by(id: params[:id])
-  #   @todo.destroy
-  #   render json: {message: "You deleted this todo"}
-  # end
+  def create
+    track_habit = TrackHabit.new(
+      habit_name: params[:habit_name],
+      habit_repeat_frequency: params[:habit_repeat_frequency],
+      habit_completion_status: params[:habit_completion_status],
+      habit_time: params[:habit_time],
+      journal_id: params[:journal_id],
+    )
+    if track_habit.save
+      render json: {message: 'habit created successfully'}, status: :created
+    else
+      render json: {errors: track_habit.errors.full_messages}, status: :bad_request
+    end
+  end
+  def update
+    @track_habit = TrackHabit.find_by(id: params[:id])
+    @track_habit.update(
+      habit_name: params[:habit_name],
+      habit_repeat_frequency: params[:habit_repeat_frequency],
+      habit_completion_status: params[:habit_completion_status],
+      habit_time: params[:habit_time],
+      journal_id: params[:journal_id],
+    )
+    render "show.json.jbuilder"
+  end
+  def destroy
+    @track_habit = TrackHabit.find_by(id: params[:id])
+    @track_habit.destroy
+    render json: {message: "You deleted this habit"}
+  end
 end
 
-
-
-   # "habit_name"
-   # "habit_repeat_frequency"
-   # "habit_completion_status"
-   # "habit_time"
-   # "journal_id"
